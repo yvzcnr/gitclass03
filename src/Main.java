34b0e981a1ed1e9fbc179faba76626dbc652808d
yavuzcinar <yvzgzd17022020@gmail.com>
2023-08-03 02:22:37 +0300

    the code that tells me the avability of the product

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+    boolean product=true;
+    if (product) {
+        System.out.println("yes");
+    }else{
+        System.out.println("no");
+    }
+    }
+}
